78d5aeea098ed4af241719e3912ad9d9f464b1a2
zchpit <rabiniak.piotr@gmail.com>
2016-03-09 00:09:34 +0100

    sscjs compleated

---
+++ sccjs/closure.js
@@ -0,0 +1,11 @@
+function foo(){
+  var bar;
+  quux = 0;
+  return zip;
+  function zip(){
+    if(true){
+       var quux = 42;
+       bar = true;
+    }
+  }
+}
---
+++ sccjs/shadowing.js
@@ -0,0 +1,9 @@
+function foo(){
+  var bar;
+  quux = 0;
+  function zip(){
+    if(true){
+       var quux = 42;
+    }
+  }
+}
