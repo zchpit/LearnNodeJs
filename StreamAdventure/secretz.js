fdd47a57b2bca7a92547d9963a4b285442b5f108
zchpit <rabiniak.piotr@gmail.com>
2016-03-15 00:09:25 +0100

    something is moving, kill it with fire

--- StreamAdventure/secretz.js
+++ StreamAdventure/secretz.js
@@ -1,7 +1,15 @@
+var algorithm = process.argv[2];
+var password = process.argv[3];
+var crypto = require('crypto');
+var zlib = require('zlib');
 var tar = require('tar');
+
 var parser = tar.Parse();
 parser.on('entry', function (e) {
     console.dir(e);
 });
 var fs = require('fs');
-fs.createReadStream('file.tar').pipe(parser);
+//fs.createReadStream(process.stdin).pipe(parser);
+var decrypt = crypto.createDecipher(algorithm, password);
+
+process.stdin.pipe(decrypt).pipe(zlib.createGunzip()).pipe(parser).pipe(process.stdout);
