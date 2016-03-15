a331ef635acd77215c7b407f40422abef5f1bbd4
zchpit <rabiniak.piotr@gmail.com>
2016-03-15 22:51:12 +0100

    cat annotation

--- Electron/index.js
+++ Electron/index.js
@@ -1,1 +1,5 @@
-require('cat-picture')
+var picture = require('cat-picture')
+var src = picture.src
+picture.remove()
+var image = require('lightning-image-poly')
+var viz = new image('#visualization', null, [src], {hullAlgorithm: 'convex'})
